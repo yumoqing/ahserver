eeedf01598c9c6244e66cfbd5e7fb0159a42f773
yumoqing <yumoqing@gmail.com>
2019-08-21 09:28:58 +0800

    push

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+# usage:
+# python setup.py bdist_wininst generate a window executable file
+# python setup.py bdist_egg generate a egg file
+# Release information about eway
+
+version = "0.0.1"
+name = "ahserver"
+description = "ahserver"
+author = "yumoqing"
+email = "yumoqing@gmail.com"
+
+packages=find_packages()
+package_data = {}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=name,
+    version=version,
+    # uncomment the following lines if you fill them out in release.py
+    description=description,
+    author=author,
+    author_email=email,
+   
+    install_requires=[
+    ],
+    packages=packages,
+    package_data=package_data,
+    keywords = [
+    ],
+	url="https://github.com/yumoqing/ahserver",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+    classifiers = [
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+    ],
+)
